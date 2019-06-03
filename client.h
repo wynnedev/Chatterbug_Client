15d74d29a0e72300a1fccd4abbf5f537e7f396af
Wynne Nicholas <44558733+wynnedev@users.noreply.github.com>
2019-06-03 10:41:16 -0400

    added comments

--- client.h
+++ client.h
@@ -19,17 +19,33 @@ class Client : public QTcpSocket
 
 public:
     explicit Client(QObject *parent = nullptr);
+    
+    // Connect client to host at specified address
     Q_INVOKABLE void clientConnect(QString address);
+    
+    // return _data
     Q_INVOKABLE QString getData();
+    
+    // Disconnect client from host
     Q_INVOKABLE void clientDisconnect();
+    
+    // Write to port
     Q_INVOKABLE void clientWrite(QString);
+    
+    // not needed delete
     void init();
 
 signals:
+
+// emit signal when new data is ready
    void newData();
 
 public slots:
+
+// Read socket
     void readClient();
+    
+    // response to established connection
     void connected();
 
 private:
